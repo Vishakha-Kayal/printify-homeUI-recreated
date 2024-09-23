5e38674acf0eba940c6f9c980b9e780f289a0548
VishakhaKayal <mgpsvishakha7944@gmail.com>
2024-09-24 00:13:13 +0530

    changed font color

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <div className='min-h-screen container'>Features</div>
+    <div className='min-h-screen container'>
+      
+    </div>
   )
 }
 
--- src/components/HeroTagline.jsx
+++ src/components/HeroTagline.jsx
@@ -1,31 +1,36 @@
-import React from 'react'
-import Button from './Button';
+import React from "react";
+import Button from "./Button";
 
 const HeroTagline = () => {
-return (
+  return (
     <section className="flex items-center justify-between px-10 py-20 font-sora h-full w-full">
       <div className="w-full">
-        <h1 className="text-8xl  leading-[7.5rem] font-semibold">Create and Sell Custom Products</h1>
+        <h1 className="text-8xl leading-[7.5rem] font-semibold bg-gradient-to-b from-[#6C5DD4] to-[#000000] bg-clip-text text-transparent">
+          Create and Sell Custom Products
+        </h1>
+
         <ul className="my-4 text-[1.6rem] font-sora mt-9">
-          <li className='mt-3'>✅ 100% Free to use</li>
-          <li className='mt-3'>✅ 900+ High-Quality Products</li>
-          <li className='mt-3'>✅ Largest global print network</li>
+          <li className="mt-3">✅ 100% Free to use</li>
+          <li className="mt-3">✅ 900+ High-Quality Products</li>
+          <li className="mt-3">✅ Largest global print network</li>
         </ul>
-        <div className='w-[23%] mt-9'>
-        <Button
-              bg="bg-[#6C5DD4]"
-              paddingY="py-[1.25rem]"
-              paddingX="px-[1.35rem]"
-              text="text-2xl"
-              font="font-bold"
-              color="text-white"
-              content="Start for free"
-            />
+        <div className="w-[23%] mt-9">
+          <Button
+            bg="bg-[#6C5DD4]"
+            paddingY="py-[1.25rem]"
+            paddingX="px-[1.35rem]"
+            text="text-2xl"
+            font="font-bold"
+            color="text-white"
+            content="Start for free"
+          />
         </div>
-        <h2 className="text-[#6C5DD4] mt-8 tag">Trusted by over 8M sellers around the world</h2>
+        <h2 className="text-[#6C5DD4] mt-8 tag">
+          Trusted by over 8M sellers around the world
+        </h2>
       </div>
     </section>
   );
-}
+};
 
-export default HeroTagline
+export default HeroTagline;
